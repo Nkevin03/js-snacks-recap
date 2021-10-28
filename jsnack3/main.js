7d772f07e07019a854597656b241e8ff19cee178
Nkevin03 <knezosi6@gmail.com>
2021-10-28 21:03:06 +0200

    Commit correzione

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,5 +1,7 @@
-const interi = ['1', '2', '3', '4','5', '6', '7', '8', '9', '10',]
-
-for (i = 1; i < interi.length; i = i + 2){
-    console.log(interi[i]);
+const interi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let somma = 0
+for (i = 1; i < interi.length; i = i+ 2){
+    somma = somma + interi[i]
 }
+
+console.log(somma);
